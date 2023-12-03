6a3a603232850af37ced04cf76c16d06607ad906
unknown <szhokee@gmail.com>
2023-12-03 18:54:08 +0600

    str2

--- str.py
+++ str.py
@@ -1,6 +1,27 @@
-txt = "banana"
-x = txt.center(20, "-")
+# txt = "banana"
+# x = txt.center(20, "-")
+# print(x)
+
+#___________________________________________________
+
+# sentence = "Hello, welcome to my world."
+# print(sentence.endswith("."))
+
+#___________________________________________________
+
+# sentence = "H\te\tl\tl\to"
+
+# print(sentence.expandtabs(1))
+# print(sentence.expandtabs(2))
+# print(sentence.expandtabs(3))
+# print(sentence.expandtabs(4))
+# print(sentence.expandtabs(5))
+
+#___________________________________________________
+
+sentence = "Hello world"
+x = sentence.find("world")
 print(x)
 
 # Результат:
-# -------banana-------
+# 6
