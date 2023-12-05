76ecabb73c2606f20b33240936a51bb263e73379
unknown <szhokee@gmail.com>
2023-12-05 18:45:55 +0600

    day7r

--- func.py
+++ func.py
@@ -2,3 +2,16 @@ def learn_to_code():
     print("You can learn to code for free on Pythonist")
 
 learn_to_code()
+
+
+#______________________________________________________________________________
+
+def learn_to_code():
+    print("You can learn to code for free on freeCodeCamp")
+
+    def learn_what_language():
+        print("You can learn any programming language on the freeCodeCamp YouTube channel")
+    
+    learn_what_language()
+
+learn_to_code()
