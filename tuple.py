be6540bc9b69ca0162a6f0c4b4ff93f5acec510c
unknown <szhokee@gmail.com>
2023-12-11 18:37:20 +0600

    day12g

--- tuple.py
+++ tuple.py
@@ -45,3 +45,10 @@ thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
   print("Yes, 'apple' is in the fruits tuple")
 #_____________________________________________________________
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
