7a7d041b2b095e187ff7c1f23dc4d766c9146205
unknown <szhokee@gmail.com>
2023-12-23 22:08:20 +0600

    day25tv

--- ifelse.py
+++ ifelse.py
@@ -51,5 +51,12 @@ c = 500
 if a > b and c > a:
   print("Both conditions are True")
 #_________________________________________________________________
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")  
+
 #_________________________________________________________________
 
