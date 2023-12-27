d65a8eefa2257b3eeb46593b352a5e94fca293a2
unknown <szhokee@gmail.com>
2023-12-27 22:55:55 +0600

    day29sss

--- function.py
+++ function.py
@@ -50,3 +50,14 @@ print(my_function(3))
 print(my_function(5))
 print(my_function(9))
 #______________________________________________________________________________
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
