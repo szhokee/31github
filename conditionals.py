62a44cd0ea8b8d35dc33ab5bab6a2aee485a053a
unknown <szhokee@gmail.com>
2024-01-01 19:49:19 +0600

    31q

--- conditionals.py
+++ conditionals.py
@@ -67,3 +67,52 @@ set1 = {1, 2, 3, 4, 5, 6}
 for i in set1:
     print(i),  
 #______________________________________________________________________________
+
+# A simple Python program to demonstrate
+# working of iterators using an example type
+# that iterates from 10 to given value
+
+# An iterable user defined type
+class Test:
+
+	# Constructor
+	def __init__(self, limit):
+		self.limit = limit
+
+	# Creates iterator object
+	# Called when iteration is initialized
+	def __iter__(self):
+		self.x = 10
+		return self
+
+	# To move to next element. In Python 3,
+	# we should replace next with __next__
+	def __next__(self):
+
+		# Store current value ofx
+		x = self.x
+
+		# Stop iteration if limit is reached
+		if x > self.limit:
+			raise StopIteration
+
+		# Else increment and return old value
+		self.x = x + 1;
+		return x
+
+# Prints numbers from 10 to 15
+for i in Test(15):
+	print(i)
+
+# Prints nothing
+for i in Test(5):
+	print(i)
+
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
+#______________________________________________________________________________
