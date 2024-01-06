496c5d009bb2e83e04c1ae3b8bc2faf6c1cdedb7
unknown <szhokee@gmail.com>
2024-01-06 19:42:02 +0600

    0601

--- OOP.py
+++ OOP.py
@@ -64,3 +64,37 @@ a = Employee('Rahul', 886012, 200000, "Intern")
 # its instance
 a.display()
 a.details()
+
+
+#_________________________________________________________________
+
+class Bird:
+
+	def intro(self):
+		print("There are many types of birds.")
+
+	def flight(self):
+		print("Most of the birds can fly but some cannot.")
+
+class sparrow(Bird):
+
+	def flight(self):
+		print("Sparrows can fly.")
+
+class ostrich(Bird):
+
+	def flight(self):
+		print("Ostriches cannot fly.")
+
+obj_bird = Bird()
+obj_spr = sparrow()
+obj_ost = ostrich()
+
+obj_bird.intro()
+obj_bird.flight()
+
+obj_spr.intro()
+obj_spr.flight()
+
+obj_ost.intro()
+obj_ost.flight()
