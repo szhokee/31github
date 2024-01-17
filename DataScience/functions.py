45bf639c5283652d9f01187ef97bc501a8fbf81c
unknown <szhokee@gmail.com>
2024-01-17 21:45:31 +0600

    1701

---
+++ DataScience/functions.py
@@ -0,0 +1,6 @@
+def calculate_total(a, b):  # Parameters: a and b
+    total = a + b           # Task: Addition
+    return total            # Output: Sum of a and b
+
+result = calculate_total(5, 7)  # Calling the function with inputs 5 and 7
+print(result)  # Output: 12
