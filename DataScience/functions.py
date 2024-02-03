50f784aaba6e8a4f59ca150aa05073197f448e55
unknown <szhokee@gmail.com>
2024-02-03 21:54:15 +0600

    03022024

--- DataScience/functions.py
+++ DataScience/functions.py
@@ -4,3 +4,30 @@ def calculate_total(a, b):  # Parameters: a and b
 
 result = calculate_total(5, 7)  # Calling the function with inputs 5 and 7
 print(result)  # Output: 12
+
+
+def add(a, b):
+    return a + b
+
+sum_result = add(3, 5)  # sum_result gets the value 8
+
+
+def print_numbers(limit):
+    for i in range(1, limit+1):
+        print(i)
+
+print_numbers(5)  # Output: 1 2 3 4 5
+
+
+def greet(name):
+    return "Hello, " + name
+
+for _ in range(3):
+    print(greet("Alice"))
+
+
+def beer(name):
+    return'Hello? ' + name
+for i in range(3):
+    print(beer("ALICE"))
+
